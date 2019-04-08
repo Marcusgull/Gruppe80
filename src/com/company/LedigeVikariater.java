2757fb279c6635293d32891f64d37bbd9c02da79
Marcus Gullerud <marcusgull@gmail.com>
2019-04-08 15:36:13 +0200

    LedigeVikariater
    ikke helt ferdig enda

---
+++ src/com/company/LedigeVikariater.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class LedigeVikariater {
+    private String Sektor;
+    private String Sted;
+    private Arbeidsgiver Arbeidsgiver;
+    private String Jobbkategori;
+    private String Varighet;
+    private int Arbeidstid;
+    private String Stillingstype;
+    private String Kvalifikasjoner;
+
+}
