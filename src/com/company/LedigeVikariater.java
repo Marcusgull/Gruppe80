548e33ad8799e516e297323ab7956e91b6c90fe0
Gustav <gustav.wehn@hotmail.com>
2019-04-09 12:56:51 +0200

    lagt til arbeidsforhold

--- src/com/company/LedigeVikariater.java
+++ src/com/company/LedigeVikariater.java
@@ -10,4 +10,79 @@ public class LedigeVikariater {
     private String Stillingstype;
     private String Kvalifikasjoner;
 
+    public LedigeVikariater(String Sektor, String Sted, Arbeidsgiver Arbeidsgiver, String Jobbkategori, String Varighet, int Arbeidstid, String Stillingstype, String Kvalifikasjoner) {
+        this.Sektor = Sektor;
+        this.Sted = Sted;
+        this.Arbeidsgiver = Arbeidsgiver;
+        this.Jobbkategori = Jobbkategori;
+        this.Varighet = Varighet;
+        this.Arbeidstid = Arbeidstid;
+        this.Stillingstype = Stillingstype;
+        this.Kvalifikasjoner = Kvalifikasjoner;
+    }
+
+    public String getSektor() {
+        return Sektor;
+    }
+
+    public void setSektor(String Sektor) {
+        this.Sektor = Sektor;
+    }
+
+    public String getSted() {
+        return Sted;
+    }
+
+    public void setSted(String Sted) {
+        this.Sted = Sted;
+    }
+
+    public Arbeidsgiver getArbeidsgiver() {
+        return Arbeidsgiver;
+    }
+
+    public void setArbeidsgiver(Arbeidsgiver Arbeidsgiver) {
+        this.Arbeidsgiver = Arbeidsgiver;
+    }
+
+    public String getJobbkategori() {
+        return Jobbkategori;
+    }
+
+    public void setJobbkategori(String Jobbkategori) {
+        this.Jobbkategori = Jobbkategori;
+    }
+
+    public String getVarighet() {
+        return Varighet;
+    }
+
+    public void setVarighet(String Varighet) {
+        this.Varighet = Varighet;
+    }
+
+    public int getArbeidstid() {
+        return Arbeidstid;
+    }
+
+    public void setArbeidstid(int Arbeidstid) {
+        this.Arbeidstid = Arbeidstid;
+    }
+
+    public String getStillingstype() {
+        return Stillingstype;
+    }
+
+    public void setStillingstype(String Stillingstype) {
+        this.Stillingstype = Stillingstype;
+    }
+
+    public String getKvalifikasjoner() {
+        return Kvalifikasjoner;
+    }
+
+    public void setKvalifikasjoner(String Kvalifikasjoner) {
+        this.Kvalifikasjoner = Kvalifikasjoner;
+    }
+
 }
