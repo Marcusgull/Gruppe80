e98358615d35d2677b96f497e193903640898b51
Marcus Gullerud <marcusgull@gmail.com>
2019-04-23 16:20:09 +0200

    Scene builder
    Lagd ny scene builder branch, her jobber vi med interfacet.

---
+++ src/com/company/InterfaceMethods.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javafx.fxml.FXML;
+
+public class InterfaceMethods {
+
+    @FXML
+    public void initialize(){
+
+    }
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,17 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     public static void main(String[] args) {
-	System.out.println("HEI GRUPPE80");
-        System.out.println("heihei"); 
-        
-        Arbeidsgiver a1 = new Arbeidsgiver("slottet", "mat", "email@email.no", "99922999");
-        
-        System.out.println(a1.getBransje());
-        
-        System.exit(0);
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        primaryStage.show();
     }
 }
