281e849981572a01df710e4f4a14bee52fd7813d
Unknown <marcusgull@gmail.com>
2019-04-28 14:07:53 +0100

    Fjernet alt, lagt til på nytt

---
+++ src/main/java/modeller/Arbeidsgiver.java
@@ -0,0 +1,48 @@
+package modeller;
+
+
+public class Arbeidsgiver {
+    private String adresse;
+    private String bransje;
+    private String email;
+    private String tlf;
+    
+    Arbeidsgiver(String adresse, String bransje, String email, String tlf){
+        this.adresse = adresse;
+        this.bransje = bransje;
+        this.email = email;
+        this.tlf = tlf;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getBransje() {
+        return bransje;
+    }
+
+    public void setBransje(String bransje) {
+        this.bransje = bransje;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+}
